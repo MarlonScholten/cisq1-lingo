679ddfa7f313c7553713f065dd23779332258a1a
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2021-03-15 19:15:34 +0100

    setup for a new game service

--- src/main/java/nl/hu/cisq1/lingo/application/GameService.java
+++ src/main/java/nl/hu/cisq1/lingo/application/GameService.java
@@ -1,10 +1,14 @@
 package nl.hu.cisq1.lingo.application;
 
 import nl.hu.cisq1.lingo.data.dto.GameDTOStrategy;
+import nl.hu.cisq1.lingo.data.dto.GameProgressDTO;
 import nl.hu.cisq1.lingo.data.repositories.SpringGameRepository;
 import nl.hu.cisq1.lingo.domain.LingoGame;
+import nl.hu.cisq1.lingo.domain.exceptions.WordNotFoundException;
 import nl.hu.cisq1.lingo.words.data.SpringWordRepository;
+import nl.hu.cisq1.lingo.words.domain.Word;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class GameService {
@@ -16,8 +20,12 @@ public class GameService {
 		this.wordRepo = wordRepo;
 	}
 
-//	public GameDTOStrategy newGame(){
-//		LingoGame game = new LingoGame();
-//
-//	}
+	// TODO: fix the dto and write tests
+	public GameDTOStrategy newGame(){
+		LingoGame game = new LingoGame();
+		Word wordToGuess = this.wordRepo.findRandomWordByLength(LingoGame.getWordLengths().get(0))
+				.orElseThrow(() -> new WordNotFoundException());
+		game.nextRound(wordToGuess.getValue());
+		return new GameProgressDTO(0,0,0);
+	}
 }
--- src/main/java/nl/hu/cisq1/lingo/domain/LingoGame.java
+++ src/main/java/nl/hu/cisq1/lingo/domain/LingoGame.java
@@ -3,13 +3,28 @@ package nl.hu.cisq1.lingo.domain;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class LingoGame implements Serializable {
+	@Getter
+	private final static List<Integer> wordLengths = List.of(5,6,7);
+	private int prevWordLen;
 	private List<Round> rounds;
 	private int score;
 
+	public LingoGame(){
+		this.prevWordLen = wordLengths.get(0);
+		this.rounds = new ArrayList<>();
+		this.score = 0;
+	}
+
+	// Starts a new round, adds it to the rounds list and returns it
+	public Round nextRound(String wordToGuess){
+		Round round = Round.newRound(wordToGuess);
+		this.rounds.add(round);
+		return round;
+	}
 }
