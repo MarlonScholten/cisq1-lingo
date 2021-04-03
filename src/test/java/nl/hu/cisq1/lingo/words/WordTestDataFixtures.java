8c750ed641c1d16eb5590bb319744bd76b495913
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2021-04-03 17:04:57 +0200

    added some fixtures

--- src/test/java/nl/hu/cisq1/lingo/words/WordTestDataFixtures.java
+++ src/test/java/nl/hu/cisq1/lingo/words/WordTestDataFixtures.java
@@ -14,7 +14,10 @@ public class WordTestDataFixtures implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.repository.save(new Word("pizza"));
+        this.repository.save(new Word("woord"));
         this.repository.save(new Word("oranje"));
+        this.repository.save(new Word("hoeden"));
         this.repository.save(new Word("wanorde"));
+        this.repository.save(new Word("dagmenu"));
     }
 }
